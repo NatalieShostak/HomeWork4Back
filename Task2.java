4e9f221669f047df5782ba35a4ab7b361f5112c2
Natasha <natasha.shostak@gmail.com>
2019-07-04 00:39:20 +0300

    homework 4

--- Task2.java
+++ Task2.java
@@ -1,20 +1,15 @@
 public class Task2 {
     public static void drawRectangle (int width,int height){
-       for (int j=1; j<=height; j++) {
-           for (int i = 1; i <= width; i++) {
+       for (int j = 0; j <= height-1; j++) {
+           for (int i = 0; i <= width-1; i++) {
                System.out.print("+");
            }
            System.out.println();
        }
     }
 
-    public static void drawRectangle (int width){
-        for (int j=1; j<=width; j++){
-            for (int i=1; i<=width; i++) {
-                System.out.print("+");
-            }
-            System.out.println();
-        }
+    public static void drawRectangle (int width) {
+        drawRectangle(width,width);
     }
 
     public static void main(String[] args) {
